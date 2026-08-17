e0ac03e84814af600c25b3d4298ee0be7b5aa080
RachxMan <heisha.rachman3911@student.unri.ac.id>
2026-08-17 13:34:36 +0700

    feat(kelola-sesi): tambah enum StageType & StageStatus, update routes

--- routes/web.php
+++ routes/web.php
@@ -69,6 +69,14 @@ Route::middleware(['auth', 'verified'])->group(function () {
         ->name('sesi-pekerja');
     Route::get('sesi-pekerja/tambah', [SesiPekerjaController::class, 'create'])
         ->name('sesi-pekerja.create');
+    Route::post('sesi-pekerja', [SesiPekerjaController::class, 'store'])
+        ->name('sesi-pekerja.store');
+    Route::get('sesi-pekerja/{session}', [SesiPekerjaController::class, 'show'])
+        ->name('sesi-pekerja.show');
+    Route::post('sesi-pekerja/{session}/stages/{stage}/assign', [SesiPekerjaController::class, 'assignStage'])
+        ->name('sesi-pekerja.stages.assign');
+    Route::post('sesi-pekerja/{session}/stages/{stage}/complete', [SesiPekerjaController::class, 'completeStage'])
+        ->name('sesi-pekerja.stages.complete');
 
     // Logout
     Route::post('logout', [\App\Http\Controllers\Web\Auth\AuthenticatedSessionController::class, 'destroy'])
