65077132477d8532ef30038e63516f1eff707402
RachxMan <heisha.rachman3911@student.unri.ac.id>
2026-08-29 02:53:20 +0700

    fix feat access

--- resources/js/Layouts/Sidebar.tsx
+++ resources/js/Layouts/Sidebar.tsx
@@ -32,8 +32,8 @@ const menuItems: MenuItem[] = [
     { label: 'Monitoring Barang', href: '/monitoring-barang', icon: PackageSearch },
     { label: 'Monitoring Checkpoint', href: '/monitoring-cp', icon: MapPin },
     { label: 'Kelola Sesi Pekerja', href: '/sesi-pekerja', routeName: 'kelola-sesi', icon: ClipboardList, roles: ['super-admin'] },
-    { label: 'Submit Dokumen', href: '/submit-berkas', routeName: 'submit-berkas.index', icon: FileUp },
-    { label: 'Verifikasi Berkas', href: '/verifikasi-berkas', routeName: 'verifikasi-berkas', icon: FileCheck2, roles: ['supervisor'] },
+    { label: 'Submit Dokumen', href: '/submit-berkas', routeName: 'submit-berkas.index', icon: FileUp, roles: ['super-admin', 'staff'] },
+    { label: 'Verifikasi Dokumen', href: '/verifikasi-berkas', routeName: 'verifikasi-berkas', icon: FileCheck2, roles: ['super-admin', 'supervisor'] },
     { label: 'Laporan', href: '/laporan', icon: BarChart3 },
 ];
 
