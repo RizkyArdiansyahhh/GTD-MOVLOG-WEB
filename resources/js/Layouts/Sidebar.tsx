1ad49a84a9485ca8a737a67963d9412cf6927fb3
RachxMan <heisha.rachman3911@student.unri.ac.id>
2026-08-29 03:02:02 +0700

    role permission

--- resources/js/Layouts/Sidebar.tsx
+++ resources/js/Layouts/Sidebar.tsx
@@ -33,7 +33,7 @@ const menuItems: MenuItem[] = [
     { label: 'Monitoring Checkpoint', href: '/monitoring-cp', icon: MapPin },
     { label: 'Kelola Sesi Pekerja', href: '/sesi-pekerja', routeName: 'kelola-sesi', icon: ClipboardList, roles: ['super-admin'] },
     { label: 'Submit Dokumen', href: '/submit-berkas', routeName: 'submit-berkas.index', icon: FileUp, roles: ['super-admin', 'staff'] },
-    { label: 'Verifikasi Dokumen', href: '/verifikasi-berkas', routeName: 'verifikasi-berkas', icon: FileCheck2, roles: ['super-admin', 'supervisor'] },
+    { label: 'Verifikasi Dokumen', href: '/verifikasi-berkas', routeName: 'verifikasi-berkas', icon: FileCheck2, roles: ['supervisor'] },
     { label: 'Laporan', href: '/laporan', icon: BarChart3 },
 ];
 
